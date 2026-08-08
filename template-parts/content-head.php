5ddbfe9b8d53932c766c4522af0db739a10f4246
MaisaMaisa <maisaimamovic@gmail.com>
2026-08-08 15:02:50 -0700

    fonts and all that

--- template-parts/content-head.php
+++ template-parts/content-head.php
@@ -6,10 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
+    <link rel='stylesheet' href='<?php echo includes_url('css/dashicons.min.css'); ?>' />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inclusive+Sans:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
-	<link rel='stylesheet' href='<?php echo includes_url('css/dashicons.min.css'); ?>' />
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 	<?php wp_head();?>
 </head> 
 <body <?php body_class(); ?>>
